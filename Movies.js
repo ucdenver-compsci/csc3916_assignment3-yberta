97ed6001657b50b4e72d00d4874177460029e4e8
Yeabsera <yeabbert@gmail.com>
2024-04-12 21:17:53 -0600

    updated movies.js

--- Movies.js
+++ Movies.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-mongoose.connect(process.env.DB);
+try {
+    mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
 // Movie schema
 const MovieSchema = new Schema({
     title: {type: String, required: true, index: true},
