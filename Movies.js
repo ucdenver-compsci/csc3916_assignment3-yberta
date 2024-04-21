1ca7daf9ecf8d7e7f5ef0581f984ab2c23060f54
Yeabsera <yeabbert@gmail.com>
2024-04-21 00:22:37 -0600

    updated multiple

--- Movies.js
+++ Movies.js
@@ -56,37 +56,49 @@ const addMovies = [
         releaseDate: new Date('1995-09-22'),
         genre: 'Mystery',
         actors: [{actorName: "Morgan Freeman", characterName: "William Somerset"}, {actorName: "Brad Pitt", characterName: "David Mills"},{actorName:"Kevin Spacey", characterName:"John Doe"}
-        ]
+        ],
+        imageUrl : "https://assignment3react.s3.us-east-2.amazonaws.com/MV5BZDgyZmY5MmItY2I3Ny00NjA4LWFhNmItMGQ4ZGJhZDk5YjU3XkEyXkFqcGdeQXVyMTAzMDM4MjM0._V1_.jpg"
     },
     {
         title: "Scarface",
         releaseDate: new Date('1983-12-09'),
         genre: 'Thriller',
         actors: [{actorName: "Al Pacino", characterName: "Tony Montana"}, {actorName: "Steven Bauer", characterName: "Manny Ribera"}, {actorName: "Michelle Pfeiffer", characterName: "Elvira Hancock"}
-        ]
+        ],
+        imageUrl : "https://assignment3react.s3.us-east-2.amazonaws.com/MV5BNjdjNGQ4NDEtNTEwYS00MTgxLTliYzQtYzE2ZDRiZjFhZmNlXkEyXkFqcGdeQXVyNjU0OTQ0OTY%40._V1_.jpg"
     },
     {
         title: "The Sandlot",
         releaseDate: new Date('1993-04-07'),
         genre: 'Comedy',
         actors: [{actorName: "Thomas Guiry", characterName: "Scottie Smalls"}, {actorName: "Mike Vitar", characterName:"Benjamin Franklin Rodriguez"}, {actorName: "Patrick Renna", characterName:"Hamilton Porter"}
-        ]
+        ],
+        imageUrl : "https://assignment3react.s3.us-east-2.amazonaws.com/The-sandlot-scaled.jpg"
     },
     {
         title: "The Pursuit of Happyness",
         releaseDate: new Date('2006-12-15'),
         genre: 'Drama',
         actors: [{actorName: "Will Smith", characterName: "Chris Gardner"}, {actorName: "Brian Howe", characterName: "Jay Twistie"}, {actorName: "Jaden Smith", characterName: "Christopher"}
-        ]
+        ],
+        imageUrl : "https://assignment3react.s3.us-east-2.amazonaws.com/thepursuitofhappyness_onesheet_1400x2100.png"
+    },
+    {
+        title: "Inception",
+        releaseDate: new Date('2010-07-16'),
+        genre: 'Science Fiction',
+        actors: [{actorName: "Leonardo DiCaprio", characterName: "Cobb"}, {actorName: "Joseph Gordon-Levitt", characterName: "Arthur"}, {actorName: "Cillian Murphy", characterName: "Fischer"}
+        ],
+        imageUrl : "https://assignment3react.s3.us-east-2.amazonaws.com/inception.jpg"
     }
 ];
 
-/*Movie.create(addMovies, function(err, movies){
+Movie.create(addMovies, function(err, movies){
     if (err){
         console.error("Error adding movies", err);
     } else{
         console.log("Movies added", movies)
     }
-});*/
+});
 
 module.exports = Movie;
